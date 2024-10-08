081d6ff535a809f13ea7a4ad80ff0d28c115a0e9
pedro roca de togores <pedrobgrdt@gmail.com>
2024-10-08 14:21:29 +0200

    redeploy with render for the server and vercel for the front, warning message if not logged

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,21 +1,37 @@
-import { useContext } from "react"
-import { Outlet, Navigate } from "react-router-dom"
-import Loader from "../components/Loader/Loader"
-import { AuthContext } from "./../Context/Auth.context"
+import { useContext, useState, useEffect } from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import Loader from "../components/Loader/Loader";
+import { AuthContext } from "./../Context/Auth.context";
 
 const PrivateRoute = () => {
+    const { user, isLoading } = useContext(AuthContext);
+    const [showWarning, setShowWarning] = useState(false);
 
-    const { user, isLoading } = useContext(AuthContext)
+    useEffect(() => {
+        if (!user && !isLoading) {
+            setShowWarning(true);  // Muestra el mensaje de advertencia
+            // Oculta el mensaje de advertencia después de 3 segundos
+            const timer = setTimeout(() => {
+                setShowWarning(false);
+            }, 3000);
+            return () => clearTimeout(timer); // Limpia el temporizador cuando se desmonta el componente
+        }
+    }, [user, isLoading]);
 
     if (isLoading) {
-        return <Loader />
+        return <Loader />;
     }
 
     if (!user) {
-        return <Navigate to="/" />
+        return (
+            <>
+                {showWarning && <p className="warning-message">Es necesario hacer login para usar esta funcionalidad.</p>}
+                <Navigate to="/" />
+            </>
+        );
     }
 
-    return <Outlet />
-}
+    return <Outlet />;
+};
 
-export default PrivateRoute
+export default PrivateRoute;
